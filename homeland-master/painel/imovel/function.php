256d52a1f82d222ce2464173232fb9b7e133fa10
Alem <ricardolhjunior@gmail.com>
2025-04-10 20:33:45 -0300

    a

--- homeland-master/painel/imovel/function.php
+++ homeland-master/painel/imovel/function.php
@@ -3,7 +3,7 @@
 	$cd_postal, $id_tipo_imovel, $qt_suite, $qt_sala_estar, $qt_quarto, $qt_banheiro, $qt_lavabo, 
 	$qt_vaga_garagem, $qt_sala_jantar, $qt_cozinha, $ic_area_externa, $ic_piscina, $ic_edicula, $ic_churrasqueira,
 	$ds_imovel, $id_proprietario, $ds_imovel, $id_proprietario, $dt_registro_imovel, $id_usuario_registro,
-	$cd_senha, $id_tipo_usuario){
+	$cd_senha, $id_tipo_usuario): void{
 		global $con;
 		$sql = 'insert into tb_imovel set
 				nm_endereço = ?,
